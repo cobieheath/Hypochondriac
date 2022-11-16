48db71a2356e987546a96f7f7809610cf33bf794
Cobie French <chf19@fsu.edu>
2022-11-16 14:09:01 -0500

    hand, belly, knee

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
 let img;
-let button;
+let head;
+let hand;
+let belly;
+let knee;
 
 function setup() {
   createCanvas(500, 700);
   img = loadImage('Human.png')
-  button = createButton('');
-  button.position(247,85);
-  button.mousePressed(thought);
+  head = createButton('');
+  head.position(247,85);
+  head.mousePressed(alerthead);
+  hand = createButton('');
+  hand.position(136,373);
+  hand.mousePressed(alerthand);
+  belly = createButton('');
+  belly.position (247,300);
+  belly.mousePressed(alertbelly);
+  knee = createButton('');
+  knee.position (275,480);
+  knee.mousePressed(alertknee);
 }
 
 function draw() {
@@ -14,8 +26,27 @@ function draw() {
   image(img, 0, 0);
 }
 
-function thought() {
-  if (button.mousePressed) {
+function alerthead() {
+  if (head.mousePressed) {
     alert('hello');
   }
 }
+
+function alerthand() {
+  if (hand.mousePressed) {
+    alert('hello');
+  }
+}
+
+function alertbelly() {
+  if (head.mousePressed) {
+    alert('hello');
+  }
+}
+
+function alertknee() {
+  if (head.mousePressed) {
+    alert('hello');
+  }
+}
+
